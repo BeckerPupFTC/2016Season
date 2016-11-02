d1603bdf008a534ea16816720a5f8ab11291f3ed
ndwrobotics <toms_collectibles@hotmail.com>
2016-11-01 19:20:39 -0700

    color sensor update

--- ftc_app-master-Noah/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ColorSensorDriver.java
+++ ftc_app-master-Noah/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ColorSensorDriver.java
@@ -44,8 +44,6 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class ColorSensorDriver extends LinearOpMode {
 
-  public enum ColorSensorDevice {ADAFRUIT, HITECHNIC_NXT, MODERN_ROBOTICS_I2C};
-
   public ColorSensorDevice device = ColorSensorDevice.MODERN_ROBOTICS_I2C;
 
   ColorSensor colorSensor;
@@ -58,16 +56,11 @@ public class ColorSensorDriver extends LinearOpMode {
     hardwareMap.logDevices();
 
     cdim = hardwareMap.deviceInterfaceModule.get("dim");
-    switch (device) {
-      case HITECHNIC_NXT:
-        colorSensor = hardwareMap.colorSensor.get("nxt");
-        break;
-      case ADAFRUIT:
-        colorSensor = hardwareMap.colorSensor.get("lady");
-        break;
-      case MODERN_ROBOTICS_I2C:
-        colorSensor = hardwareMap.colorSensor.get("mr");
-        break;
+
+
+    colorSensor = hardwareMap.colorSensor.get("mr");
+
+
     }
     led = hardwareMap.led.get("led");
     t = hardwareMap.touchSensor.get("t");
@@ -81,17 +74,10 @@ public class ColorSensorDriver extends LinearOpMode {
 
       enableLed(t.isPressed());
 
-      switch (device) {
-        case HITECHNIC_NXT:
-          Color.RGBToHSV(colorSensor.red(), colorSensor.green(), colorSensor.blue(), hsvValues);
-          break;
-        case ADAFRUIT:
-          Color.RGBToHSV((colorSensor.red() * 255) / 800, (colorSensor.green() * 255) / 800, (colorSensor.blue() * 255) / 800, hsvValues);
-          break;
-        case MODERN_ROBOTICS_I2C:
-          Color.RGBToHSV(colorSensor.red()*8, colorSensor.green()*8, colorSensor.blue()*8, hsvValues);
-          break;
-      }
+
+      Color.RGBToHSV(colorSensor.red()*8, colorSensor.green()*8, colorSensor.blue()*8, hsvValues);
+
+
       telemetry.addData("Clear", colorSensor.alpha());
       telemetry.addData("Red  ", colorSensor.red());
       telemetry.addData("Green", colorSensor.green());
@@ -105,19 +91,9 @@ public class ColorSensorDriver extends LinearOpMode {
       });
       waitOneFullHardwareCycle();
     }
-  }
+
 
   private void enableLed(boolean value) {
-    switch (device) {
-      case HITECHNIC_NXT:
-        colorSensor.enableLed(value);
-        break;
-      case ADAFRUIT:
-        led.enable(value);
-        break;
-      case MODERN_ROBOTICS_I2C:
-        colorSensor.enableLed(value);
-        break;
-    }
+    colorSensor.enableLed(value);
   }
 }

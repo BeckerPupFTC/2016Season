fbb72a2ec4ebe4da016ab972880a3c4cacece6b4
ndwrobotics <toms_collectibles@hotmail.com>
2016-11-01 19:10:22 -0700

    Updated code

--- ftc_app-master-Noah/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9Line.java
+++ ftc_app-master-Noah/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9Line.java
@@ -32,8 +32,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
@@ -41,20 +40,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  * <p>
  * Enables control of the robot via the gamepad
  */
-public class K9Line extends OpMode {
-
-	final static double MOTOR_POWER = 0.15; // Higher values will cause the robot to move faster
-	final static double HOLD_IR_SIGNAL_STRENGTH = 0.20; // Higher values will cause the robot to follow closer
-	final static double LIGHT_THRESHOLD = 0.5;
-
-	double armPosition;
-	double clawPosition;
-
-	DcMotor motorRight;
-	DcMotor motorLeft;
-	Servo claw;
-	Servo arm;
-	LightSensor reflectedLight;
+public class K9Line extends LinearOpMode {
 
 	/**
 	 * Constructor
@@ -70,112 +56,23 @@ public class K9Line extends OpMode {
 	 */
 	@Override
 	public void init() {
+		ServoController servoController = hardwareMap.ServoController.get("Servo_Controller_1")
+		ColorSensor colorSensor = hardwareMap.ColorSensor.get("color_sensor_1");
+		Servo indicator = hardwareMap.servo.get("servo_1");
+		indicator.setPosition(0);
 
-		/*
-		 * Use the hardwareMap to get the dc motors and servos by name.
-		 * Note that the names of the devices must match the names used
-		 * when you configured your robot and created the configuration file.
-		 */
 
-		/*
-		 * For the demo Tetrix K9 bot we assume the following,
-		 *   There are two motors "motor_1" and "motor_2"
-		 *   "motor_1" is on the right side of the bot.
-		 *   "motor_2" is on the left side of the bot..
-		 *
-		 * We also assume that there are two servos "servo_1" and "servo_6"
-		 *    "servo_1" controls the arm joint of the manipulator.
-		 *    "servo_6" controls the claw joint of the manipulator.
-		 */
-		motorRight = hardwareMap.dcMotor.get("motor_2");
-		motorLeft = hardwareMap.dcMotor.get("motor_1");
-		motorLeft.setDirection(DcMotor.Direction.REVERSE);
-
-		arm = hardwareMap.servo.get("servo_1");
-		claw = hardwareMap.servo.get("servo_6");
-
-		// set the starting position of the wrist and claw
-		armPosition = 0.2;
-		clawPosition = 0.25;
-
-		/*
-		 * We also assume that we have a LEGO light sensor
-		 * with a name of "light_sensor" configured for our robot.
-		 */
-		reflectedLight = hardwareMap.lightSensor.get("light_sensor");
-
-        // turn on LED of light sensor.
-        reflectedLight.enableLed(true);
 	}
-
-	/*
-	 * This method will be called repeatedly in a loop
-	 *
-	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
-	 */
 	@Override
 	public void loop() {
-		double reflection = 0.0;
-		double left, right = 0.0;
-
-		// keep manipulator out of the way.
-		arm.setPosition(armPosition);
-		claw.setPosition(clawPosition);
-
-        /*
-         * As a temporary fix, turn on LED in run() event rather than in start().
-         */
-        // turn on LED of light sensor.
-        //reflectedLight.enableLed(true);
-
-		/*
-		 * read the light sensor.
-		 */
-		reflection = reflectedLight.getLightDetected();
-
-		/*
-		 * compare measured value to threshold.
-		 */
-		if (reflection < LIGHT_THRESHOLD) {
-			/*
-			 * if reflection is less than the threshold value, then assume we are above dark spot.
-			 * turn to the right.
-			 */
-			left = MOTOR_POWER;
-			right = 0.0;
-		} else {
-			/*
-			 * assume we are over a light spot.
-			 * turn to the left.
-			 */
-			left = 0.0;
-			right = MOTOR_POWER;
+		double reflectedLight = getAnalogInputValue(0);
+		double divisor = 100.0;
+		reflectedRed = reflectedRed/divisor;
+		if (reflectedRed > 1.0) {
+			reflectedRed = 1.0;
 		}
-
-		/*
-		 * set the motor power
-		 */
-		motorRight.setPower(left);
-		motorLeft.setPower(right);
-
-		/*
-		 * Send telemetry data back to driver station. Note that if we are using
-		 * a legacy NXT-compatible motor controller, then the getPower() method
-		 * will return a null value. The legacy NXT-compatible motor controllers
-		 * are currently write only.
-		 */
-
-		telemetry.addData("Text", "*** Robot Data***");
-		telemetry.addData("reflection", "reflection:  " + Double.toString(reflection));
-		telemetry.addData("left tgt pwr",  "left  pwr: " + Double.toString(left));
-		telemetry.addData("right tgt pwr", "right pwr: " + Double.toString(right));
+		indicator.setPosition(reflectedRed);
 	}
-
-	/*
-	 * Code to run when the op mode is first disabled goes here
-	 *
-	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
-	 */
 	@Override
 	public void stop() {
 

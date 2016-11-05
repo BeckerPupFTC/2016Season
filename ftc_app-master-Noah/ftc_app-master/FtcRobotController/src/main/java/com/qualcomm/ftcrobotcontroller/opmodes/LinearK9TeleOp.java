f2fd4f1cc4c06445e28ceb703712fde57188b3ac
ndwrobotics <toms_collectibles@hotmail.com>
2016-11-04 18:52:43 -0700

    Autonomous code for scrimmage

--- ftc_app-master-Noah/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LinearK9TeleOp.java
+++ ftc_app-master-Noah/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LinearK9TeleOp.java
@@ -29,44 +29,218 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
+/* Important:
+ * Stuff I need to check:
+ * Direction of continuous servo;
+ * Direction of intake;
+ */
+/* Code contains a couple of important comments. Please look through them.
+ * Hope this works :)
+ */
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.util.Range;
 
-/**
- * Linear Tele Op Mode
- * <p>
- * Enables control of the robot via the gamepad.
- * NOTE: This op mode will not work with the NXT Motor Controllers. Use an Nxt op mode instead.
- */
-public class LinearK9TeleOp extends LinearOpMode {
+import static java.lang.Math.abs;
 
-  DcMotor motorRight;
-  DcMotor motorLeft;
+public class WreckChecker implements Runnable {
+  int left_encoder_now = -2147483648;
+  int right_encoder_now = -2147483648;
+  int left_encoder_then;
+  int right_encoder_then;
+  volatile boolean inTrouble = false;
 
+  WreckChecker(String name) {
+    thread = new Thread(this, name);
+    thread.start();
+  }
 
-  @Override
-  public void runOpMode() throws InterruptedException {
-    motorLeft = hardwareMap.dcMotor.get("motor_1");
-    motorRight = hardwareMap.dcMotor.get("motor_2");
-
-    motorLeft.setDirection(DcMotor.Direction.REVERSE);
-
-    waitForStart();
-
-    while (opModeIsActive()) {
-
-      // write the values to the motors
-      motorRight.setPower(1);
-      motorLeft.setPower(1);
-
-      sleep(1000);
-      motorRight.setPower(0);
-      motorLeft.setPower(0);
-      sleep(10000);
-      waitOneFullHardwareCycle();
+  public void run() {
+    while (weAreRunning) {
+      left_encoder_then = left_encoder_now;
+      right_encoder_then = right_encoder_now;
+      try
+      {
+        Thread.sleep(200);
+      }
+      catch (Exception e){}
+      left_encoder_now = motorLeft.getPosition();
+      right_encoder_now = motorRight.getPosition();
+      if (left_encoder_then != -2147483648) {
+        if ((motor_right.isBusy() && (abs(right_encoder_now - right_encoder_then) < 400)) || (motor_left.isBusy() && (abs(left_encoder_now - left_encoder_then) < 50))) {
+          left_encoder_then = left_encoder_now;
+          right_encoder_then = right_encoder_now;
+          try
+          {
+            Thread.sleep(200);
+          }
+          catch (Exception e){}
+          left_encoder_now = motorLeft.getPosition();
+          right_encoder_now = motorRight.getPosition();
+          if ((motor_right.isBusy() && (abs(right_encoder_now - right_encoder_then) < 400)) || (motor_left.isBusy() && (abs(left_encoder_now - left_encoder_then) < 50))) {
+            inTrouble = true;
+            try {
+              Thread.sleep(300);
+            } catch (Exception e) {}
+            inTrouble = false;
+          }
+        }
+      }
     }
   }
 }
+
+public class LinearK9TeleOp extends LinearOpMode {
+  //initialization
+  DcMotorController wheelController;
+  DcMotor motorRight;
+  DcMotor motorLeft;
+
+  DcMotorController launcherController;
+  DcMotor intake;
+  DcMotor launcher;
+
+  ServoController servoBox;
+  Servo intakeServo;
+
+  volatile boolean weAreRunning = true;
+
+  WreckChecker wreckChecker;
+
+  public double motorMax = 0.97;
+	/* Encoder software assumes max rpm
+	 * of motor is 156; ours is 152, so I set it to this to
+	 * not over power the motor.
+	 */
+
+  @Override
+  public void runOpMode()
+  {
+    //More initialization
+    wheelController = hardwareMap.dcMotorController.get("wheel controller");
+    motorLeft = hardwareMap.dcMotor.get("left wheel");
+    motorRight = hardwareMap.dcMotor.get("right wheel");
+
+    launcherController = hardwareMap.dcMotorController.get("launcher controller");
+    intake = hardwareMap.dcMotor.get("intake");
+    launcher = hardwareMap.dcMotor.get("launcher");
+
+    servoBox = hardwareMap.servoController.get("servo box")
+    intakeServo = hardwareMap.servo.get("servo 1");
+
+    motorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    motorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    waitForStart();
+
+    wreckChecker = new WreckChecker("wreck checker");
+
+    goDirection(33.5,0);
+    intakeServo.setPosition(1);// If servo spins backwards, change to 0
+    launcher.setPower(1);// If launcher spins backwards, change to -1
+    try
+    {
+      Thread.sleep(2000);
+    }
+    catch (Exception e) {}
+    intakeServo.setPosition(0.5);
+    launcher.setPower(0);
+    goDirection(20,90);
+    goDirection(22,-90);
+    goDirection(14,-90);
+    try
+    {
+      Thread.sleep(1000);
+    }
+    catch (Exception e){}
+    goDirection(5,0);
+  }
+
+  public void goDirection(double inches, double degrees)
+  {
+    boolean noError = true;
+		/* For the variable direction, 0 makes the robot go straight, 90 right, -90 left,
+		* -180 makes a 180 turn to the left, 180 makes a 180 turn to the right. Larger numbers will
+		* make the robot spin in circles.
+		*/
+    int turnAmount = direction * 27.75; //27.75 encoder units in 1 degree
+
+    leftTarget = motorLeft.getPosition() + turnAmount;
+    rightTarget = motorRight.getPosition() - turnAmount;
+
+    motorLeft.setTargetPosition(leftTarget);
+    motorRight.setTargetPosition(rightTarget);
+
+    motorLeft.setPower(motorMax);
+    motorRight.setPower(motorMax);
+    while(motorLeft.isBusy() || motorRight.isBusy())
+    {
+      if (inTrouble)
+      {
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+
+        leftNow = motorLeft.getPosition();
+        rightNow = motorRight.getPosition();
+        if (direction < 0)
+        {
+          goDirection(6,1);
+        }
+        if (direction > 0)
+        {
+          goDirection(6,-1);
+        }
+
+        try
+        {
+          Thread.sleep(500);
+        }
+        catch (Exception e) {}
+
+        noError = false;
+      }
+    }
+    double distance = inches * 229.183;
+
+    if (noError)
+    {
+      motorLeft.setTargetPosition(motorLeft.getPosition() + distance);
+      motorRight.setTargetPosition(motorRight.getPosition() + distance);
+      motorLeft.setPower(motorMax);
+      motorRight.setPower(motorMax);
+
+      while (motorLeft.isBusy() || motorRight.isBusy())
+      {
+        if(inTrouble)
+        {
+          motorLeft.setPower(0);
+          motorRight.setPower(0);
+
+          leftNow = motorLeft.getPosition();
+          rightNow = motorRight.getPosition();
+
+          goDirection(-6,0);
+
+          try
+          {
+            Thread.sleep(1000);
+          }
+          catch (Exception e) {}
+
+          noError = false;
+        }
+      }
+    }
+    motorLeft.setPower(0);
+    motorRight.setPower(0);
+  }
+}
+//4995 encoder units/360 degrees
+/* 2pi inches for 1440 encoder units. 1 inch is 229.183 units */
+//4995 encoder units/360 degrees
+/* 2pi inches for 1440 encoder units. 1 inch is 229.183 units
